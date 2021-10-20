99a4ba8471fdb38742bb978f752cd3c6d7502d2f
Ryan T <rtruong@umich.edu>
2021-10-20 19:01:38 -0400

    inital

---
+++ Pack.cpp
@@ -0,0 +1,92 @@
+  //Project UID 1d9f47bfc76643019cfbf037641defe1
+  #include "Pack.h"
+  #include "Card.h"
+  #include <iostream>
+
+
+
+
+  Pack::Pack(){
+      
+    std::array<std::string,8> Ranks = {Card::RANK_NINE, Card::RANK_TEN, Card::RANK_JACK,
+            Card::RANK_QUEEN,Card::RANK_KING, Card::RANK_ACE};
+    std::array<std::string,4> Suits = {Card::SUIT_SPADES,Card::SUIT_HEARTS,
+    Card::SUIT_CLUBS, Card::SUIT_DIAMONDS};
+for(int c = 0; c<4; c++){
+    for(int i = 0; i<6; i++){
+        
+            cards[((i))+(c*6)] = Card(Ranks[i],Suits[c]);
+        }
+    }
+    next = 0;
+    
+  }
+
+  // REQUIRES: pack_input contains a representation of a Pack in the
+  //           format required by the project specification
+  // MODIFIES: pack_input
+  // EFFECTS: Initializes Pack by reading from pack_input.
+  Pack::Pack(std::istream& pack_input){
+    std::string suit;
+    std::string null;
+    std::string rank;
+    int i = 0;
+
+    pack_input >> rank >> null >> suit;
+    do{
+    cards[i]=Card(rank,suit);
+    pack_input >> rank >> null >> suit;
+    i++;
+    }while(i<24);
+    next = 0;
+
+  }
+
+  // REQUIRES: cards remain in the Pack
+  // EFFECTS: Returns the next card in the pack and increments the next index
+  Card Pack::deal_one(){
+    return cards[next++];
+  }
+
+  // EFFECTS: Resets next index to first card in the Pack
+  void Pack::reset(){
+      next = 0;
+  }
+
+  // EFFECTS: Shuffles the Pack and resets the next index. This
+  //          performs an in shuffle seven times. See
+  //          https://en.wikipedia.org/wiki/In_shuffle.
+  void print_card_array(std::array<Card,24> &card){
+      for(int i = 0; i<24; i++){
+          std::cout << i << ": " << card[i] << std::endl;
+      }
+  }
+  void slice(std::array<Card,24> &cards,std::array<Card,12> &half, bool front_or_back){
+      int offset = front_or_back ? 0 : 12;
+      for(int i = offset; i<offset+12; ++i){
+          half[i-offset] = cards[i];
+      }
+  }
+  void Pack::shuffle(){
+      next = 0;
+      print_card_array(cards);
+      std::array<Card, PACK_SIZE/2> first_half;
+      std::array<Card, PACK_SIZE/2> second_half;
+      for(int i = 0; i<7; ++i){
+        slice(cards,first_half,true);
+        slice(cards,second_half,false);
+        //print_card_array(first_half);
+        //print_card_array(second_half);
+        for(int c = 0; c<24; ++c){
+            cards[c] = (c+1)%2==0 ? first_half[(c-1)/2] : second_half[(c)/2];
+        }
+        std::cout << "round " << i << std::endl;
+        print_card_array(cards);
+      }
+      
+  }
+
+  // EFFECTS: returns true if there are no more cards left in the pack
+  bool Pack::empty() const{
+      return next == PACK_SIZE; 
+  }
---
+++ euchre.cpp
@@ -0,0 +1,62 @@
+// Project UID 1d9f47bfc76643019cfbf037641defe1
+#include <iostream>
+#include <fstream>
+
+#include "Card.h"
+#include "Pack.h"
+#include "Player.h"
+
+
+
+
+
+using namespace std;
+
+int main(int argc, char * argv[]){
+
+    ifstream f(argv[1]);
+    if(argc!=12){
+        cout << "please enter 12 arguments";
+        return 1;
+    }
+    if(atoi(argv[3])<1 || atoi(argv[3])>100){
+        cout << "please enter score between 1 and 100";
+        return 1;
+    }
+
+    
+    array<Player *, 4> Players;
+    if(f.is_open()){ // if open and santized run
+        Pack pack(f);
+        f.close();
+
+        bool shuffle =  argv[2]=="shuffle" ? true : false;
+        int win_threshold =  atoi(argv[3]);
+        
+        
+        for(int i = 0; i<4; i++){ // create players to spec
+            Players[i] = Player_factory(argv[4+i*2],argv[5+i*2]);
+        }
+        for(int i = 0; i<4; i++){
+            for(int i = 0; i<4; i++){ // create players to spec
+                Players[i]->add_card(pack.deal_one());
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }else{
+        cout << "please enter a valid pack in file";
+        return 1;
+    }
+
+    for(int i = 0; i<4; i++) delete Players[i];
+    
+    return 0;
+}
