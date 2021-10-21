6d3be86e3e8c19bd2ea216d2e23efaef233f12b9
Gabe Price <starfinderjetpacks@gmail.com>
2021-10-21 18:35:15 -0400

    work plz

--- euchre.cpp
+++ euchre.cpp
@@ -134,7 +134,7 @@ int main(int argc, char * argv[]){
         //cout << "test lead " << (leader)%4 << endl;
         for(int p = 1; p < 4; ++p){
             trick[p] = Players[(leader+p)%4]->play_card(trick[0],trump);
-            for(int i = 0; i < 5; ++i) {
+            for(int i = 0; i < 4; ++i) {
                 cout << "player " << Players[(leader+p)%4]->get_name() << 
                 "'s hand: [" << i << "] " << Players[(leader+p)%4]->card(i) << endl;
             }
