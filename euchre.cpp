5f243d8e3891aabb3b3f8a56ab9653bd93986e0f
Gabe Price <starfinderjetpacks@gmail.com>
2021-10-21 18:14:54 -0400

    merge

--- euchre.cpp
+++ euchre.cpp
@@ -27,59 +27,15 @@ void deal(Pack &pack, array<Player *, 4> Players, int dealer){
         }
 }
 
-int index_of_highest_value_card(array<Card,4> &trick, string &trump, Card &lead){
-    int index = 0;
-    if(Card_less(trick[0],trick[1],lead, trump)) index = 1;
-
-    int index2 = 2;
-    if(Card_less(trick[2],trick[3],lead,trump)) index2 = 3;
-
-    if(Card_less(trick[index],trick[index2],lead,trump)) index = index2;
-
-    return index;
-}
-
-int main(int argc, char * argv[]){
-
-    ifstream f(argv[1]);
-    if(argc!=12){
-        cout << "please enter 12 arguments";
-        return 1;
-    }
-    if(atoi(argv[3])<1 || atoi(argv[3])>100){
-        cout << "please enter score between 1 and 100";
-        return 1;
-    }
-
-    array<Player *, 4> Players;
-    Card upcard;
-    string trump = ""; 
-    int dealer = 0;
-    int h = 0;
-    int hand = 0;
-
-    if(!f.is_open()) {
-        string pack_filename = argv[1];
-        cout << "Error opening " << pack_filename << endl;
-        return 1; // return nonzero number
-    }
-
-    Pack pack(f);
-    f.close();
-    string s = argv[2];
-    bool shuffle = s=="shuffle" ? true : false;
-    if(shuffle) pack.shuffle();
-    //int win_threshold =  atoi(argv[3]);
-    
+void createPlayers(array<Player *, 4> &Players, char * argv[]){
     for(int i = 0; i<4; i++){ // create players to spec
         Players[i] = Player_factory(argv[4+i*2],argv[5+i*2]);
     }
+}
 
-    deal(pack, Players, dealer);
-    trump = ""; 
-    h = 0;
-    hand = 0;
-
+int makeTrump(array<Player *, 4> &Players, Pack &pack, int hand, int dealer, string &Trump, Card &upcard){
+    string trump = ""; 
+    int h = 0;
     upcard = pack.deal_one();
     cout << "Hand " << hand << endl;
     cout << *Players[dealer] << " deals" << endl;
@@ -115,6 +71,59 @@ int main(int argc, char * argv[]){
         trump = s;
 
     }
+    return 0;
+}
+
+int index_of_highest_value_card(array<Card,4> &trick, string &trump, Card &lead){
+    int index = 0;
+    if(Card_less(trick[0],trick[1],lead, trump)) index = 1;
+
+    int index2 = 2;
+    if(Card_less(trick[2],trick[3],lead,trump)) index2 = 3;
+
+    if(Card_less(trick[index],trick[index2],lead,trump)) index = index2;
+
+    return index;
+}
+
+int main(int argc, char * argv[]){
+
+    ifstream f(argv[1]);
+    if(argc!=12){
+        cout << "please enter 12 arguments";
+        return 1;
+    }
+    if(atoi(argv[3])<1 || atoi(argv[3])>100){
+        cout << "please enter score between 1 and 100";
+        return 1;
+    }
+
+    array<Player *, 4> Players;
+    Card upcard;
+    string trump = ""; 
+    int dealer = 0;
+    int hand = 0;
+
+    if(!f.is_open()) {
+        string pack_filename = argv[1];
+        cout << "Error opening " << pack_filename << endl;
+        return 1; // return nonzero number
+    }
+
+    Pack pack(f);
+    f.close();
+    string s = argv[2];
+    bool shuffle = s=="shuffle" ? true : false;
+    if(shuffle) pack.shuffle();
+    //int win_threshold =  atoi(argv[3]);
+    
+    createPlayers(Players, argv);
+
+    deal(pack, Players, dealer);
+    
+    hand = 0;
+
+    makeTrump(Players, pack, hand, dealer, trump, upcard);
     
     int leader = dealer+1;
     array<Card, 4> trick = {};
